4f022f0625f35ad23f75231619aee992d7a23e90
Adrian Castañeda Rodriguez <ing.adrian.castaneda@gmail.com>
2024-03-12 17:25:27 -0700

    general Colors

---
+++ vasflix_app/lib/config/theme_app.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const Color caribbean = Color(0xFF15616D);
+const Color white = Color(0xFFFFFFFF);
+const Color navy = Color(0xFF001524);
+const Color black = Color(0xFF000000);
+const Color blueGray = Color(0xFF415A77);
+const Color errorRed = Color(0xFFD90429);
+const Color likeRed = Color(0xFFef233c);
+const Color warning = Color(0xFFff8800);
+const Color successGreen = Color(0xFF38b000);
+const Color lightBlue = Color(0xFF16B3CB);
